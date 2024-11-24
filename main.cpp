4c4e71ac8349299b6729ddfc6381b2c57c20abb6
Vphome2003 <vago.peter100@gmail.com>
2024-11-24 22:12:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' // ""
-    for (int i = 0;) // i< N_ELEMENTS; i++
+    int *b = new int[N_ELEMENTS]; // N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa" // ""
+    for (int i = 0;i< N_ELEMENTS; i++) // i< N_ELEMENTS; i++
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // i< N_ELEMENTS;
+    for (int i = 0;i< N_ELEMENTS; i++) // i< N_ELEMENTS;
     {
-        std::cout << "Ertek:" // << b[i]<< std::endl;
+        std::cout << "Ertek:"<< b[i]<< std::endl; // << b[i]<< std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // double atlag = 0;
+    double atlag = 0; // double atlag = 0;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i] // ;
+        atlag += b[i]; // ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
